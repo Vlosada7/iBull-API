6c6f8a4f4fb47a37e23a6ecd0de74d5198503ee3
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-06-16 15:04:45 +0200

    Routes and controllers working

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
-	console.log(`🚀 Server is listening on port PORT!`);
+	console.log(`🚀 Server is listening on port ${PORT}!`);
 });
 export default app;
