1ea41749902abbc685c43b906aae6bf341d1e70c
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-06-16 17:04:15 +0200

    Get trade and Get trade with query working

--- src/Controllers/tradeControllers.ts
+++ src/Controllers/tradeControllers.ts
@@ -42,7 +42,33 @@ const postTrade = async (req: Request, res: Response) => {
 
 
 const getTrade = async (req: Request, res: Response) => {
-	res.status(200).send("get Trade working");
+  const { type, user_id } = req.query;
+  let trades;
+  if (type && user_id) {
+    try {
+      trades = await TradeModel.find({ type, user_id }).exec();
+      if (trades.length === 0) {
+        res.status(404).send({message: "Trade not found"});
+      } else {
+        res.status(200).send(trades);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Unexpected API error" });
+    }
+  } else {
+    try {
+      trades= await TradeModel.find().exec();
+      if (trades.length === 0) {
+        res.status(200).send({message: "Not trades yer"});
+      } else {
+        res.status(200).send(trades);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Unexpected API error" });
+    }
+  }
 };
 
 const getTradeById = async (req: Request, res: Response) => {
